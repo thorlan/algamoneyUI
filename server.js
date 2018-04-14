c5ff679a47ffa94ed7fca04f6f14a1d0872a6899
Thiago Orlandini <thiagorlandini@hotmail.com>
2018-04-14 18:29:52 -0300

    deploy2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req, res){
+app.get('/*', function(req, res) {
   res.sendFile(__dirname + '/dist/index.html');
 });
 
